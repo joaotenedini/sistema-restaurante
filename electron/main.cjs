d33bfbc9ec70d4d1469f0964f97f7794cee7eaab
joao vitor <mkt.tenedini@gmail.com>
2025-01-27 10:11:49 -0300

    feat: adiciona suporte ao Electron e configuração de build

---
+++ electron/main.cjs
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { autoUpdater } = require('electron-updater');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Em desenvolvimento, carrega o servidor local
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    // Em produção, carrega os arquivos buildados
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  // Configurar atualizações automáticas
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ electron/main.js
@@ -0,0 +1,40 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { autoUpdater } from 'electron-updater';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Em desenvolvimento, carrega o servidor local
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    // Em produção, carrega os arquivos buildados
+    mainWindow.loadFile(join(__dirname, '../dist/index.html'));
+  }
+
+  // Configurar atualizações automáticas
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
